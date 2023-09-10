26f23610cc25bc02347c69e69aa6e9e36b4e7cbc
santr4 <santra.aaranyak16092003@gmail.com>
2023-09-10 12:01:06 +0530

    program to generate pascals triangle

---
+++ Arrays/tut1.java
@@ -0,0 +1,28 @@
+// Program to generate Pascals triangle 
+
+import java.util.*;
+
+public class tut1 {
+    public static long nCr(int n, int r) {
+        long res = 1;
+
+        // calculating nCr
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+
+    public static void pascaltriangle(int n){
+        // printing the entire row n
+        for(int c = 1;c <= n;c++){
+            System.out.println(nCr(n-1,c-1) + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        pascaltriangle(n);
+    }
+}
