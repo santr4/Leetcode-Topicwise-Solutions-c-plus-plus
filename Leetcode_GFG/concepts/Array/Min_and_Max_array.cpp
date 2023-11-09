a13db503f7d24451a5eae413b01fdc0ee04492a7
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2023-11-09 11:28:48 +0530

    min and max of an array

--- Leetcode_GFG/concepts/Array/Min_and_Max_array.cpp
+++ Leetcode_GFG/concepts/Array/Min_and_Max_array.cpp
@@ -1,7 +1,39 @@
 // ** Minimum and Maximum of an array using minimum number of operations.
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// struct Pair
+// {
+//     int min;
+//     int max;
+// };
+
+// Pair getMinMax(int arr[], int n)
+// {
+//     Pair minmax;
+
+//     sort(arr, arr + n);
+
+//     minmax.min = arr[0];
+//     minmax.max = arr[n - 1];
+
+//     return minmax;
+// }
+
+// int main()
+// {
+//     int arr[] = {12, 3, 0, 88, 100, 1};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     Pair minmax = getMinMax(arr, n);
+
+//     cout << "the minimum element in the array is: " << minmax.min << endl;
+//     cout << "the maximum element in the array is: " << minmax.max << endl;
+//     return 0;
+// }
+
+// ** Min and Max of an array using linear search
 
 struct Pair
 {
@@ -11,24 +43,51 @@ struct Pair
 
 Pair getMinMax(int arr[], int n)
 {
-    Pair minmax;
+    struct Pair minmax;
 
-    sort(arr, arr + n);
+    if (n == 1)
+    {
+        minmax.min = arr[0];
+        minmax.max = arr[1];
+        return minmax;
+    }
 
-    minmax.min = arr[0];
-    minmax.max = arr[n - 1];
+    if (arr[0] > arr[1])
+    {
+        minmax.min = arr[1];
+        minmax.max = arr[0];
+    }
+    else if (arr[0] <= arr[1])
+    {
+        minmax.min = arr[0];
+        minmax.max = arr[1];
+    }
 
+    for (int i = 2; i < n; i++)
+    {
+        if (arr[i] > minmax.max)
+        {
+            minmax.max = arr[i];
+        }
+        else if (arr[i] < minmax.min)
+        {
+            minmax.min = arr[i];
+        }
+    }
     return minmax;
 }
 
+#include <bits/stdc++.h>
+using namespace std;
+
 int main()
 {
-    int arr[] = {12, 3, 0, 88, 100, 1};
+    int arr[] = {5, 8, 3, 2, 0, 8, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     Pair minmax = getMinMax(arr, n);
 
-    cout << "the minimum element in the array is: " << minmax.min << endl;
-    cout << "the maximum element in the array is: " << minmax.max << endl;
+    cout << "the minimum element of the array is: " << minmax.min << endl;
+    cout << "the maximum element of the array is: " << minmax.max << endl;
     return 0;
 }
