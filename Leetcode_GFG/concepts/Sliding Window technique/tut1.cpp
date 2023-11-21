9911563c78a2389f34d2d155773e1961b6377b02
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2023-11-21 16:18:46 +0530

    sliding window technique

--- Leetcode_GFG/concepts/Sliding Window technique/tut1.cpp
+++ Leetcode_GFG/concepts/Sliding Window technique/tut1.cpp
@@ -30,3 +30,42 @@
 //     cout << maxsum(arr, n, k);
 //     return 0;
 // }
+
+// ** sliding window approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSum(int arr[], int n, int k)
+{ // returns maximum sum from subarray of size of k
+    if (n < k)
+    {
+        cout << "invalid" << endl;
+        return -1;
+    }
+
+    // compute sum of first window of size k
+    int max_sum = 0;
+    for (int i = 0; i < k; i++)
+    {
+        max_sum += arr[i];
+    }
+
+    int window_sum = max_sum;
+    for (int i = k; i < n; i++)
+    {
+        window_sum += (arr[i] - arr[i - k]);
+        max_sum = max(max_sum, window_sum);
+    }
+
+    return max_sum;
+}
+
+int main()
+{
+    int arr[] = {1, 4, 2, 10, 2, 3, 1, 0, 20};
+    int k = 4;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << maxSum(arr, n, k);
+    return 0;
+}
