10f2b40741770b5bc6a1fa2706312066bdf327f3
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2023-11-23 22:38:21 +0530

    20. Valid Parentheses leetcode

---
+++ Leetcode_GFG/concepts/Stack/tut1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    stack<int> stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.pop();
+    while (!stack.empty())
+    {
+        cout << stack.top() << endl;
+        stack.pop();
+    }
+    return 0;
+}
